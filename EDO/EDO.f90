e7c4e7bcf7347948f37e26b10afe729eba856244
Wenceslao <wenceslaomateos@gmail.com>
2019-08-27 15:34:54 -0300

    Agregado de f

---
+++ EDO/EDO.f90
@@ -0,0 +1,18 @@
+module EDOs
+    implicit none
+    
+contains
+
+    function f(x)
+        implicit none
+        real(8) x, f
+        
+        f = 2*X
+    end function f
+    
+end module EDOs
+
+program principal
+    implicit none
+    
+end program principal
