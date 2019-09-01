88ba49645d72f817b95728b1c9e2d97d57c535ca
Wenceslao <wenceslaomateos@gmail.com>
2019-09-01 16:29:12 -0300

    Metodos de EDO primera clase funcionando
    Co-authored-by: Bravlin <bravlinleonel@gmail.com>

--- EDO/EDO.f90
+++ EDO/EDO.f90
@@ -13,7 +13,7 @@ contains
         intent (in) :: x, y
         real(8) x, y, fp
 
-        fp = 2*x !Cambiar funcion aca
+        fp = 2. * x !Cambiar funcion aca
     end function fp
 
     function eulerSimple(x, y, h)
@@ -22,7 +22,47 @@ contains
         
         eulerSimple = y + h * fp(x, y)
     end function eulerSimple
-    
+
+    function eulerModificado(x, y, h)
+        intent (in) :: x, y, h
+        real(8) x, h, y, yaux, eulerModificado
+
+        yaux = y + h * fp(x, y)        
+        eulerModificado = y + h * ((fp(x, y) + fp(x + h, yaux)) / 2.)
+    end function eulerModificado
+
+    function eulerMejorado(x, y, h)
+        intent (in) :: x, y, h
+        real(8) x, h, y, yaux, eulerMejorado
+
+        yaux = y + (h / 2.) * fp(x, y)        
+        eulerMejorado = y + h * fp(x + (h / 2.), yaux)
+    end function eulerMejorado
+
+    function rk(x, y, h)
+        intent (in) :: x, y, h
+        real(8) x, h, y, k1, k2, k3, k4, rk
+
+        k1 = h * fp(x, y)
+        k2 = h * fp(x + h / 2., y + k1 / 2.)
+        k3 = h * fp(x + h / 2., y + k2 / 2.)
+        k4 = h * fp(x + h, y + k3)
+        rk = y + (1. / 6.) * (k1 + 2. * k2 + 2. * k3 + k4)
+    end function rk
+
+    function rkf(x, y, h)
+        intent (in) :: x, y, h
+        real(8) x, h, y, k1, k2, k3, k4, k5, k6, rkf
+
+        k1 = h * fp(x, y)
+        k2 = h * fp(x + h/4., y + k1/4.)
+        k3 = h * fp(x + 3.*h/8., y + (3.*k1 + 9.*k2)/32.)
+        k4 = h * fp(x + 12.*h/13., y + (1932.*k1 - 7200.*k2 + 7296.*k3)/2197)
+        k5 = h * fp(x + h, y + (439.*k1/216.) - 8.*k2 + (3680.*k3/513.) - (845.*k4/4104.))
+        k6 = h * fp(x + h/2., y - 8.*k1/27. + 2.*k2 - 3544.*k3/2565. - 1859.*k4/4104. - 11.*k5/40.)
+        rkf = y + 25.*k1/216. + 1408.*k3/2565. + 2197.*k4/4104. - k5/5.
+    end function rkf
+
 end module EDOs
 
 program principal
@@ -40,8 +80,8 @@ program principal
     y = 25
     open(2, file='datos.dat')
     do i = 1, repeticiones
-        y = eulerSimple(x, y, h)
         write(2,'(2F15.10)') x, y
+        y = rkf(x, y, h)
         x = x + h
     end do
     close(2)
