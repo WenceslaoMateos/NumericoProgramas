5eb4eed486477508585ce1a15e3e2af3861772de
Bravlin <bravlinleonel@gmail.com>
2019-09-08 21:51:10 -0300

    Arreglado parametro innecesario

--- EDO/EDO_Vectorizado_Enlace_Tardio.f90
+++ EDO/EDO_Vectorizado_Enlace_Tardio.f90
@@ -25,12 +25,13 @@ module EDOs
     end interface
 
     abstract interface
-        function cambioPaso(met, v, h, tol, fp)
+        subroutine pasoConCambio(met, v, h, tol, fp)
             procedure(metodo) :: met
             procedure(derivada) :: fp
-            real(8), intent(in) :: v(0:), h, tol
-            real(8) cambioPaso
-        end function cambioPaso
+            intent(in) :: tol
+            intent(inout) :: v, h
+            real(8) :: v(0:), h, tol
+        end subroutine pasoConCambio
     end interface
 
     interface iterar
@@ -99,45 +100,48 @@ contains
     end function rkf
     
 !-----------------------------------------CAMBIO DE PASO--------------------------------------------------!
-    function estrategia1(met, v, h, tol, fp)
+    subroutine estrategia1(met, v, h, tol, fp)
         procedure(metodo) :: met
         procedure(derivada) :: fp
-        real(8), intent(in) :: v(0:), h, tol
-        real(8) estrategia1
+        intent(in) :: tol
+        intent(inout) :: v, h
+        real(8) v(0:), h, tol
         real(8) vsig(0:size(v) - 1), vsig2(0:size(v) - 1)
 
-        estrategia1 = h
-        vsig = met(v, estrategia1, fp)
-        vsig2 = met(met(v, estrategia1/2., fp), estrategia1/2., fp)
+        vsig = met(v, h, fp)
+        vsig2 = met(met(v, h/2., fp), h/2., fp)
         do while (maxval(abs(vsig - vsig2)) > tol)
-            estrategia1 = estrategia1/2.
-            vsig = met(v, estrategia1, fp)
-            vsig2 = met(met(v, estrategia1/2, fp), estrategia1/2., fp)    
-            write(*,*) estrategia1
+            h = h/2.
+            vsig = met(v, h, fp)
+            vsig2 = met(met(v, h/2., fp), h/2., fp)
         end do
-    end function estrategia1
+        v = vsig
+    end subroutine estrategia1
 
-    function estrategia2(met, v, h, tol, fp)
+    subroutine estrategia2(met, v, h, tol, fp)
         procedure(metodo) :: met
         procedure(derivada) :: fp
-        real(8), intent(in) :: v(0:), h, tol
+        intent(in) :: tol
+        intent(inout) :: v, h
+        real(8) v(0:), h, tol
         real(8), dimension(0:size(v) - 1) :: k1, k2, k3, k4, k5, k6, e
-        real(8) estrategia2, alfa, error
+        real(8) alfa, error
 
-!        k1 = h*fp(v)
-!        k2 = h*fp(v + k1/4.0)
-!        k3 = h*fp(v + (3.0*k1 + 9.0*k2)/32.0)
-!        k4 = h*fp(v + (1932.0*k1 - 7200.0*k2 + 7296.0*k3)/2197.0)
-!        k5 = h*fp(v + 439.0*k1/216.0 - 8.0*k2 + 3680.0*k3/513.0 - 845.0*k4/4104.0)
-!        k6 = h*fp(v - 8.0*k1/27.0 + 2.0*k2 - 3544.0*k3/2565.0 + 1859.0*k4/4104.0 - 11.0*k5/40.0)
-!        e = k1/360.0 - 128.0*k3/4275.0 - 2197.0*k4/75240.0 + k5/50.0 + 2.0*k6/55.0
-!        error = maxval(abs(e))
-!        if (error > tol) then
-!            alfa = 0.22
-!        else
-!            alfa = 0.2
-!        end if
-!        estrategia2 = h * (tol/error)**alfa
+        ! k1 = h*fp(v)
+        ! k2 = h*fp(v + k1/4.0)
+        ! k3 = h*fp(v + (3.0*k1 + 9.0*k2)/32.0)
+        ! k4 = h*fp(v + (1932.0*k1 - 7200.0*k2 + 7296.0*k3)/2197.0)
+        ! k5 = h*fp(v + 439.0*k1/216.0 - 8.0*k2 + 3680.0*k3/513.0 - 845.0*k4/4104.0)
+        ! k6 = h*fp(v - 8.0*k1/27.0 + 2.0*k2 - 3544.0*k3/2565.0 + 1859.0*k4/4104.0 - 11.0*k5/40.0)
+        ! e = k1/360.0 - 128.0*k3/4275.0 - 2197.0*k4/75240.0 + k5/50.0 + 2.0*k6/55.0
+        ! error = maxval(abs(e))
+        ! if (error > tol) then
+        !     alfa = 0.22
+        ! else
+        !     alfa = 0.2
+        ! end if
+        ! h = h * ((tol/error)**alfa)
+        ! v = met(v, h, fp)
 
         k1 = h*fp(v)
         k2 = h*fp(v + k1/4.0)
@@ -147,20 +151,20 @@ contains
         k6 = h*fp(v - 8.0*k1/27.0 + 2.0*k2 - 3544.0*k3/2565.0 + 1859.0*k4/4104.0 - 11.0*k5/40.0)
         e = k1/360.0 - 128.0*k3/4275.0 - 2197.0*k4/75240.0 + k5/50.0 + 2.0*k6/55.0
         error = maxval(abs(e))
-        estrategia2 = h
         do while (error > tol)
             alfa = 0.22
-            estrategia2 = estrategia2 * (tol/error)**alfa
-            k1 = estrategia2*fp(v)
-            k2 = estrategia2*fp(v + k1/4.0)
-            k3 = estrategia2*fp(v + (3.0*k1 + 9.0*k2)/32.0)
-            k4 = estrategia2*fp(v + (1932.0*k1 - 7200.0*k2 + 7296.0*k3)/2197.0)
-            k5 = estrategia2*fp(v + 439.0*k1/216.0 - 8.0*k2 + 3680.0*k3/513.0 - 845.0*k4/4104.0)
-            k6 = estrategia2*fp(v - 8.0*k1/27.0 + 2.0*k2 - 3544.0*k3/2565.0 + 1859.0*k4/4104.0 - 11.0*k5/40.0)
+            h = h * (tol/error)**alfa
+            k1 = h*fp(v)
+            k2 = h*fp(v + k1/4.0)
+            k3 = h*fp(v + (3.0*k1 + 9.0*k2)/32.0)
+            k4 = h*fp(v + (1932.0*k1 - 7200.0*k2 + 7296.0*k3)/2197.0)
+            k5 = h*fp(v + 439.0*k1/216.0 - 8.0*k2 + 3680.0*k3/513.0 - 845.0*k4/4104.0)
+            k6 = h*fp(v - 8.0*k1/27.0 + 2.0*k2 - 3544.0*k3/2565.0 + 1859.0*k4/4104.0 - 11.0*k5/40.0)
             e = k1/360.0 - 128.0*k3/4275.0 - 2197.0*k4/75240.0 + k5/50.0 + 2.0*k6/55.0
             error = maxval(abs(e))
         end do
-    end function estrategia2
+        v = met(v, h, fp)
+    end subroutine estrategia2
 
 !--------------------------------DEFINICIÓN DE ITERACIONES------------------------------------------------!
 !--------------------------------Sin cambio de paso---------------------------------!
@@ -200,42 +204,40 @@ contains
     end subroutine iterarValorFinal
 
 !--------------------------------Con cambio de paso---------------------------------!
-    subroutine iterarVecesTolerancia(met, fp, vinicial, h, cambioH, tol, rep, archivo)
-        intent(in) :: vinicial, h, rep, archivo, tol
+    subroutine iterarVecesTolerancia(met, fp, vinicial, hinicial, pasoVariante, tol, rep, archivo)
+        intent(in) :: vinicial, hinicial, rep, archivo, tol
         procedure(metodo) :: met
         procedure(derivada) :: fp
-        procedure(cambioPaso) :: cambioH
+        procedure(pasoConCambio) :: pasoVariante
         integer(4) rep, i
-        real(8) h, hnuevo, tol, vinicial(0:), v(0:size(vinicial) - 1)
+        real(8) h, hinicial, tol, vinicial(0:), v(0:size(vinicial) - 1)
         character (LEN=*) :: archivo
 
         open(2, file=archivo)
         v = vinicial
-        hnuevo = h
+        h = hinicial
         write(2, *) v
         do i = 1, rep
-            hnuevo = cambioH(met, v, hnuevo, tol, fp)
-            v = met(v, hnuevo, fp)
+            call pasoVariante(met, v, h, tol, fp)
             write(2, *) v
         end do
         close(2)
     end subroutine iterarVecesTolerancia
 
-    subroutine iterarValorFinalTolerancia(met, fp, vinicial, h, cambioH, tol, xf, archivo)
-        intent(in) :: vinicial, h, xf, archivo, tol
+    subroutine iterarValorFinalTolerancia(met, fp, vinicial, hinicial, pasoVariante, tol, xf, archivo)
+        intent(in) :: vinicial, hinicial, xf, archivo, tol
         procedure(metodo) :: met
         procedure(derivada) :: fp
-        procedure(cambioPaso) :: cambioH
-        real(8) h, hnuevo, tol, xf, vinicial(0:), v(0:size(vinicial) - 1)
+        procedure(pasoConCambio) :: pasoVariante
+        real(8) h, hinicial, tol, xf, vinicial(0:), v(0:size(vinicial) - 1)
         character (LEN=*) :: archivo
 
         open(2, file=archivo)
         v = vinicial
-        hnuevo = h
+        h = hinicial
         write(2, *) v
         do while (v(0) <= xf)
-            hnuevo = cambioH(met, v, hnuevo, tol, fp)
-            v = met(v, hnuevo, fp)
+            call pasoVariante(met, v, h, tol, fp)
             write(2, *) v
         end do
         close(2)
@@ -249,7 +251,7 @@ program principal
     implicit none
 
     integer(4), parameter :: cant_ec = 1
-    character(len=*), parameter :: archivo = "EDO/datos.dat"
+    character(len=*), parameter :: archivo = "datos.dat"
 
     integer(4) repeticiones
     real(8) v(0:cant_ec), h, tol, xf
@@ -258,17 +260,17 @@ program principal
     h = 1.
     v(0) = -5.
     v(1) = 25.
-    tol = 0.000000001
-    xf = 10.
-!    call iterar(eulerSimple, fp, v, h, estrategia1, tol, xf, archivo)
-!    call plot(archivo)
-!    call iterar(eulerModificado, fp, v, h, estrategia1, tol, xf, archivo)
-!    call plot(archivo)
-!    call iterar(eulerMejorado, fp, v, h, estrategia1, tol, xf, archivo)
-!    call plot(archivo)
-!    call iterar(rk, fp, v, h, estrategia1, tol, xf, archivo)
-!    call plot(archivo)
-    call iterar(rkf, fp, v, h, estrategia2, tol, repeticiones, archivo)
+    tol = 0.01
+    xf = 20.
+    call iterar(eulerSimple, fp, v, h, estrategia1, tol, xf, archivo)
+    call plot(archivo)
+    call iterar(eulerModificado, fp, v, h, estrategia1, tol, xf, archivo)
+    call plot(archivo)
+    call iterar(eulerMejorado, fp, v, h, estrategia1, tol, xf, archivo)
+    call plot(archivo)
+    call iterar(rk, fp, v, h, estrategia1, tol, xf, archivo)
+    call plot(archivo)
+    call iterar(rkf, fp, v, h, estrategia2, tol, xf, archivo)
     call plot(archivo)
     
 contains
