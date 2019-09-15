ff4cee9c774b2598d0b8e359682022c144c1c788
Bravlin <bravlinleonel@gmail.com>
2019-09-15 20:25:38 -0300

    Matriz inversa y solucion de Gauss Jordan

--- SEL/SELs.f90
+++ SEL/SELs.f90
@@ -5,15 +5,15 @@ module SELs
 contains
 
 function matrizAmpliada(matriz, term_ind)
-    real(8), dimension(:,:), intent(in) :: matriz, term_ind
+    real(8), dimension(:, :), intent(in) :: matriz, term_ind
     real(8) matrizAmpliada(size(matriz, dim=1), size(matriz, dim=2) + size(term_ind, dim=2))
     integer(4) col_mat, col_amp
 
     col_mat = size(matriz, dim=2)
     col_amp = size(matriz, dim=2) + size(term_ind, dim=2)
 
-    matrizAmpliada(:, :col_mat) = matriz(:, :)
-    matrizAmpliada(:, col_mat + 1:col_amp) = term_ind(:, :)                
+    matrizAmpliada(:, :col_mat) = matriz
+    matrizAmpliada(:, col_mat + 1:col_amp) = term_ind                
 end function matrizAmpliada
 
 function gauss(matriz, term_ind)
@@ -47,8 +47,44 @@ function gaussJordan(matriz, term_ind)
             gaussJordan(i, j) = 0.0
         end do
     end do 
-        
 end function gaussJordan
+
+function solucionGaussJordan(matriz, term_ind)
+    real(8), dimension(:, :), intent(in) :: matriz, term_ind
+    real(8) solucionGaussJordan(size(matriz, dim=1), size(matriz, dim=2) + size(term_ind, dim=2))
+    integer(4) i, filas
+
+    solucionGaussJordan = gaussJordan(matriz, term_ind)
+    filas = size(matriz, dim=1)
+    do i = 1, filas
+        solucionGaussJordan(i, :) = solucionGaussJordan(i, :) / solucionGaussJordan(i, i)
+        solucionGaussJordan(i, i) = 1.
+    end do
+end function solucionGaussJordan
+
+function identidad(orden)
+    integer(4), intent(in) :: orden
+    integer(4) i
+    real(8) identidad(orden, orden)
+
+    identidad = 0.
+    do i = 1, orden
+        identidad(i, i) = 1.
+    end do
+end function identidad
+
+function matrizInversa(matriz)
+    intent(in) :: matriz
+    integer(4) orden
+    real(8), allocatable :: aux(:, :)
+    real(8) matriz(:, :), matrizInversa(size(matriz, dim=1), size(matriz, dim=2))
+
+    orden = size(matriz, dim=1)
+    allocate(aux(orden, orden*2))
+    aux = solucionGaussJordan(matriz, identidad(orden))
+    matrizInversa = aux(:, orden + 1:)
+    deallocate(aux)
+end function
 
 end module SELs
 
@@ -58,34 +94,24 @@ program principal
 
     implicit none
 
-    integer, parameter :: orden = 4, cant_sis = 1
-    real(8) matriz(orden, orden), term_ind(orden,cant_sis), aux(orden, orden+cant_sis)
+    integer, parameter :: orden = 3
+    real(8) matriz(orden, orden), inversa(orden, orden)
 
-
-    matriz(1,1) = 2.1756
-    matriz(1,2) = 4.0231
-    matriz(1,3) = -2.1732
-    matriz(1,4) = 5.1967
-    matriz(2,1) = -4.0231
-    matriz(2,2) = 6.0000
-    matriz(2,3) = 0.
-    matriz(2,4) = 1.1973
-    matriz(3,1) = -1.0000
-    matriz(3,2) = -5.2107
-    matriz(3,3) = 1.1111
-    matriz(3,4) = 0.
-    matriz(4,1) = 6.0235
-    matriz(4,2) = 7.0000
-    matriz(4,3) = 0.
-    matriz(4,4) = -4.1561
-    term_ind(1,1) = 17.102
-    term_ind(2,1) = -6.1593
-    term_ind(3,1) = 3.0004
-    term_ind(4,1) = 0.0000
-    aux = gauss(matriz, term_ind)
-    call mostrarMatriz(aux, orden, orden+cant_sis)
-    !el resultado es 1, 2 ,3
-
+    matriz(1, 1) = 1.
+    matriz(1, 2) = 0.
+    matriz(1, 3) = 0.
+    matriz(2, 1) = -1.
+    matriz(2, 2) = 2.
+    matriz(2, 3) = 3.
+    matriz(3, 1) = 0.
+    matriz(3, 2) = 1.
+    matriz(3, 3) = 2.
+    inversa = matrizInversa(matriz)
+    call mostrarMatriz(matriz, orden, orden)
+    write(*, *)
+    call mostrarMatriz(inversa, orden, orden)
+    write(*, *)
+    call mostrarMatriz(matrizInversa(inversa), orden, orden)
 contains
 
 end program principal
