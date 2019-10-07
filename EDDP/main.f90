919b9ca3179a3f091d764c5eb0b3a584bc41000b
Bravlin <bravlinleonel@gmail.com>
2019-10-06 21:48:39 -0300

    Fix matriz distribucion

--- EDDP/main.f90
+++ EDDP/main.f90
@@ -10,8 +10,8 @@ program principal
     real(8), dimension(1:n-2) :: superior, inferior
     real(8), dimension(1:m-2) :: izquierda, derecha
     real(8), dimension(1:orden, 1) :: term_ind, xini, res
-    real(8), dimension(n+1, m+1) :: distribucion
-    integer(4) i, j
+    real(8), dimension(m, n) :: distribucion
+    integer(4) i, j, offset
 
     superior = 0.
     inferior = 0.
@@ -22,24 +22,25 @@ program principal
     xini = 0.
     res = gaussSeidel(mat, term_ind, xini, 0.000000000001_8)
     call mostrarMatriz(res)
+
+    ! Esquinas
     distribucion(1, 1) = 0.
-    distribucion(1, m) = 100.
-    distribucion(n, 1) = 0.
-    distribucion(n, m) = 100.
-    do i = 2, n-1
-        distribucion(i, 1) = izquierda(i-1)
-        distribucion(i, m) = derecha(i-1)
-    end do
-    do j = 2, m-1
-        distribucion(1, j) = superior(j-1)
-        distribucion(n, j) = inferior(j-1)
-    end do
-    do i = 1, n
-        do j = 1, m
-            distribucion(i+1, j+1) = res(i+j-1, 1)
-        end do
+    distribucion(1, n) = 100.
+    distribucion(m, 1) = 0.
+    distribucion(m, n) = 100.
+
+    ! Bordes
+    distribucion(1, 2:n-1) = superior
+    distribucion(m, 2:n-1) = inferior
+    distribucion(2:m-1, 1) = izquierda
+    distribucion(2:m-1, n) = derecha
+    offset = 0
+    do i = 2, m - 1
+        distribucion(i, 2:n-1) = res(1+offset:n-2, 1)
+        offset = offset + n - 2
     end do
 
+    call mostrarMatriz(distribucion, '(9F10.2)')
     call grabarMatriz(distribucion,'valores.dat')
     call system('gnuplot -persist matriz.p')
 
