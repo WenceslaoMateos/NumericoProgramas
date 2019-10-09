582b801fa61c7ed6467e9425fbc8e3437917f323
Bravlin <bravlinleonel@gmail.com>
2019-10-09 19:07:54 -0300

    h y k pueden ser distintos

--- EDDP/EDDP.f90
+++ EDDP/EDDP.f90
@@ -71,4 +71,77 @@ module EDDP
 
     end subroutine generaMatriz
     
+    subroutine generaMatriz2(mat, term_ind, x0, x1, y0, y1, nx, my, superior, inferior, izquierda, derecha)
+        integer(4), intent(in) :: nx, my
+        real(8), intent(in) :: x0, x1, y0, y1
+        real(8), dimension(:,:), intent(out) :: mat, term_ind
+        real(8), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
+        integer(4) filas, columnas, desde, hasta, i, n, m
+        real(8) h, k
+
+        h = (x1 - x0) / nx
+        k = (y1 - y0) / my
+        n = nx - 1
+        m = my - 1
+        mat = 0.
+        term_ind = 0. * h**2. * k**2
+        filas = size(mat, dim=1)
+        columnas = size(mat, dim=2)
+
+        ! ---------------GENERACION DE TERMINOS INDEPENDIENTES--------------- !
+        ! Condiciones superiores
+        desde = 1
+        hasta = n
+        term_ind(desde:hasta, 1) = term_ind(desde:hasta, 1) - superior(:) * h**2.
+
+        ! Condiciones inferiores
+        desde = n * (m - 1) + 1
+        hasta = n * m
+        term_ind(desde:hasta, 1) = term_ind(desde:hasta, 1) - inferior(:) * h**2.
+
+        ! Condiciones izquierda
+        desde = 1
+        hasta = n * (m - 1) + 1
+        do i = desde, hasta, n
+            term_ind(i, 1) = term_ind(i, 1) - izquierda(mod(i, n)+1) * k**2.
+        end do
+        
+        ! Condiciones derecha
+        desde = n
+        hasta = n * m     
+        do i = desde, hasta, n
+            term_ind(i, 1) = term_ind(i, 1) - derecha(i/n) * k**2.
+        end do
+
+        ! ----------------------GENERACION DE DIAGONAL---------------------- !
+        do i = 1, n * m
+            mat(i, i) = -2. * (h**2. + k**2.)
+        end do
+
+        ! ------------------------GENERACION DE UNOS------------------------ !
+        ! Banda superiores
+        do i = n + 1, n * m
+            mat(i, i - n) = h**2.
+        end do
+
+        ! Banda inferiores
+        do i = 1, n * (m - 1)
+            mat(i, i + n) = h**2.
+        end do
+
+        ! Banda izquierda
+        do i = 2, n * m
+            if (mod(i, n) /= 1) then
+                mat(i, i - 1) = k**2.
+            end if
+        end do
+
+        ! Banda derecha
+        do i = 1, n * m - 1
+            if (mod(i, n) /= 0) then
+                mat(i, i + 1) = k**2.
+            end if
+        end do
+    end subroutine
+
 end module EDDP
--- EDDP/main.f90
+++ EDDP/main.f90
@@ -5,61 +5,77 @@ program principal
 
     implicit none
     
-    integer(4), parameter :: n = 9, m = 5, orden = (n-2)*(m-2)
-    real(8), dimension(1:orden, 1:orden) :: mat
-    real(8), dimension(1:n-2) :: superior, inferior
-    real(8), dimension(1:m-2) :: izquierda, derecha
-    real(8), dimension(1:orden, 1) :: term_ind, xini, res
-    real(8), dimension(m, n) :: distribucion
-    integer(4) i, offset
+    real(8) x0, x1, y0, y1
+    integer(4) i, offset, n, m, orden
+    real(8), dimension(:, :), allocatable :: mat, distribucion
+    real(8), dimension(:), allocatable :: superior, inferior, izquierda, derecha
+    real(8), dimension(:, :), allocatable :: term_ind, xini, res
 
+    x0 = 0.
+    x1 = 20.
+    y0 = 0.
+    y1 = 10.
+    n = 140
+    m = 60
+    orden = (n - 1) * (m - 1)
+
+    allocate(mat(1:orden, 1:orden), distribucion(1:m+1, 1:n+1))
+    allocate(term_ind(1:orden, 1), xini(1:orden, 1), res(1:orden, 1))
+    allocate(superior(1:n+1), inferior(1:n+1))
+    allocate(izquierda(1:m+1), derecha(1:m+1))
     superior = 0.
     inferior = 0.
     izquierda = 0.
     derecha = 100.
-    call generaMatriz(mat, term_ind, n-2, m-2, superior, inferior, izquierda, derecha)
+
+    call generaMatriz2(mat, term_ind, x0, x1, y0, y1, n, m, superior, inferior, izquierda, derecha)
+    ! call mostrarMatriz(mat)
+    ! write (*, *)
+    ! call mostrarMatriz(term_ind)
+    ! write (*, *)
     
     xini = 0.
-    res = gaussSeidel(mat, term_ind, xini, 0.000000000001_8)
+    res = gaussSeidel(mat, term_ind, xini, 0.000001_8)
     call mostrarMatriz(res)
+    write (*, *)
 
     ! Esquinas
     distribucion(1, 1) = 0.
-    distribucion(1, n) = 100.
-    distribucion(m, 1) = 0.
-    distribucion(m, n) = 100.
+    distribucion(1, n+1) = 100.
+    distribucion(m+1, 1) = 0.
+    distribucion(m+1, n+1) = 100.
 
     ! Bordes
-    distribucion(1, 2:n-1) = superior
-    distribucion(m, 2:n-1) = inferior
-    distribucion(2:m-1, 1) = izquierda
-    distribucion(2:m-1, n) = derecha
+    distribucion(1, 2:n) = superior
+    distribucion(m+1, 2:n) = inferior
+    distribucion(2:m, 1) = izquierda
+    distribucion(2:m, n+1) = derecha
     offset = 0
-    do i = 2, m - 1
-        distribucion(i, 2:n-1) = res(1+offset:n-2, 1)
-        offset = offset + n - 2
+    do i = 2, m
+        distribucion(i, 2:n) = res(1+offset:n-1, 1)
+        offset = offset + n - 1
     end do
 
-    call mostrarMatriz(distribucion, '(9F10.2)')
-    call grabarMatriz(distribucion, 'valores.dat')
+    ! call mostrarMatriz(distribucion)
     call generarDatos(distribucion, 0._8, 20._8, 0._8, 10._8, n, m, 'valores.dat')
     call system('gnuplot -persist matriz.p')
 
+    deallocate(mat, distribucion, superior, inferior, izquierda, derecha, term_ind, xini, res)
 contains
 
     subroutine generarDatos(distribucion, x0, x1, y0, y1, n, m, archivo)
         intent(in) :: distribucion, x0, x1, y0, y1, n, m, archivo
         integer(4) n, m, i, j
-        real(8) distribucion(m, n), x0, x1, y0, y1, h, k, x, y
+        real(8) distribucion(:, :), x0, x1, y0, y1, h, k, x, y
         character(len=*) archivo
 
         open(unit=2, file=archivo, access='SEQUENTIAL', status='REPLACE')
-        h = (x1 - x0) / (n - 1)
-        k = (y1 - y0) / (m - 1)
+        h = (x1 - x0) / n
+        k = (y1 - y0) / m
         y = y0
-        do i = 1, m
+        do i = 1, m + 1
             x = x0
-            do j = 1, n
+            do j = 1, n + 1
                 write(2, *) x, y, distribucion(i, j)
                 x = x + h
             end do
