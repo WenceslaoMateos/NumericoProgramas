446329dd684874bae71af7c388bc708a0d1914cd
Bravlin <bravlinleonel@gmail.com>
2019-10-09 20:44:34 -0300

    Pulido general

--- EDDP/EDDP.f90
+++ EDDP/EDDP.f90
@@ -2,76 +2,8 @@ module EDDP
     implicit none
     
     contains
-
-    subroutine generaMatriz(mat, term_ind, n, m, superior, inferior, izquierda, derecha)
-        real(8), dimension(:,:), intent(out) :: mat, term_ind
-        real(8), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
-        integer(4), intent(in) :: n, m
-        integer(4) filas, columnas, desde, hasta, i
-
-        mat = 0.
-        term_ind = 0.
-        filas = size(mat, dim=1)
-        columnas = size(mat, dim=2)
-
-        ! ---------------GENERACION DE TERMINOS INDEPENDIENTES--------------- !
-        ! Condiciones superiores
-        desde = 1
-        hasta = n
-        term_ind(desde:hasta, 1) = term_ind(desde:hasta, 1) + superior(:)
-
-        ! Condiciones inferiores
-        desde = n * (m - 1) + 1
-        hasta = n * m
-        term_ind(desde:hasta, 1) = term_ind(desde:hasta, 1) + inferior(:)
-
-        ! Condiciones izquierda
-        desde = 1
-        hasta = n * (m - 1) + 1
-        do i = desde, hasta, n
-            term_ind(i, 1) = term_ind(i, 1) + izquierda(mod(i, n)+1)
-        end do
-        
-        ! Condiciones derecha
-        desde = n
-        hasta = n*m     
-        do i = desde, hasta, n
-            term_ind(i, 1) = term_ind(i, 1) + derecha(i/n)
-        end do
-
-        ! ----------------------GENERACION DE DIAGONAL---------------------- !
-        do i = 1, n * m
-            mat(i, i) = 4.
-        end do
-
-        ! ------------------------GENERACION DE UNOS------------------------ !
-        ! Banda superiores
-        do i = n + 1, n * m
-            mat(i, i - n) = -1.
-        end do
-
-        ! Banda inferiores
-        do i = 1, n * (m - 1)
-            mat(i, i + n) = -1.
-        end do
-
-        ! Banda izquierda
-        do i = 2, n * m
-            if (mod(i, n) /= 1) then
-                mat(i, i - 1) = -1.
-            end if
-        end do
-
-        ! Banda derecha
-        do i = 1, n * m - 1
-            if (mod(i, n) /= 0) then
-                mat(i, i + 1) = -1.
-            end if
-        end do
-
-    end subroutine generaMatriz
     
-    subroutine generaMatriz2(mat, term_ind, x0, x1, y0, y1, nx, my, superior, inferior, izquierda, derecha)
+    subroutine generarSistema(mat, term_ind, x0, x1, y0, y1, nx, my, superior, inferior, izquierda, derecha)
         integer(4), intent(in) :: nx, my
         real(8), intent(in) :: x0, x1, y0, y1
         real(8), dimension(:,:), intent(out) :: mat, term_ind
@@ -142,6 +74,55 @@ module EDDP
                 mat(i, i + 1) = k**2.
             end if
         end do
+    end subroutine generarSistema
+
+    function generarDistribucion(nx, my, resul, si, sd, ii, id, superior, inferior, izquierda, derecha)
+        integer(4), intent(in) :: nx, my
+        real(8), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
+        real(8), intent(in) :: resul(1:(nx - 1) * (my - 1)), si, sd, ii, id
+        real(8) generarDistribucion(1:my+1, 1:nx+1)
+        integer(4) i, offset
+
+        ! Esquinas
+        generarDistribucion(1, 1) = si
+        generarDistribucion(1, nx+1) = sd
+        generarDistribucion(my+1, 1) = ii
+        generarDistribucion(my+1, nx+1) = id
+
+        ! Bordes
+        generarDistribucion(1, 2:nx) = superior
+        generarDistribucion(my+1, 2:nx) = inferior
+        generarDistribucion(2:my, 1) = izquierda
+        generarDistribucion(2:my, nx+1) = derecha
+
+        ! Interno
+        offset = 0
+        do i = 2, my
+            generarDistribucion(i, 2:nx) = resul(1+offset:nx-1)
+            offset = offset + nx - 1
+        end do
+    end function generarDistribucion
+
+    subroutine grabarDatos(distribucion, x0, x1, y0, y1, nx, my, archivo)
+        intent(in) :: distribucion, x0, x1, y0, y1, nx, my, archivo
+        integer(4) nx, my, i, j
+        real(8) distribucion(:, :), x0, x1, y0, y1, h, k, x, y
+        character(len=*) archivo
+
+        open(unit=2, file=archivo, access='SEQUENTIAL', status='REPLACE')
+        h = (x1 - x0) / nx
+        k = (y1 - y0) / my
+        y = y0
+        do i = 1, my + 1
+            x = x0
+            do j = 1, nx + 1
+                write(2, *) x, y, distribucion(i, j)
+                x = x + h
+            end do
+            write(2, *)
+            y = y + k
+        end do
+        close(2, status='KEEP')
     end subroutine
 
 end module EDDP
--- EDDP/main.f90
+++ EDDP/main.f90
@@ -6,7 +6,7 @@ program principal
     implicit none
     
     real(8) x0, x1, y0, y1
-    integer(4) i, offset, n, m, orden
+    integer(4) n, m, orden
     real(8), dimension(:, :), allocatable :: mat, distribucion
     real(8), dimension(:), allocatable :: superior, inferior, izquierda, derecha
     real(8), dimension(:, :), allocatable :: term_ind, xini, res
@@ -15,20 +15,20 @@ program principal
     x1 = 20.
     y0 = 0.
     y1 = 10.
-    n = 140
-    m = 60
+    n = 40
+    m = 20
     orden = (n - 1) * (m - 1)
 
     allocate(mat(1:orden, 1:orden), distribucion(1:m+1, 1:n+1))
     allocate(term_ind(1:orden, 1), xini(1:orden, 1), res(1:orden, 1))
-    allocate(superior(1:n+1), inferior(1:n+1))
-    allocate(izquierda(1:m+1), derecha(1:m+1))
+    allocate(superior(1:n-1), inferior(1:n-1))
+    allocate(izquierda(1:m-1), derecha(1:m-1))
+
     superior = 0.
     inferior = 0.
     izquierda = 0.
     derecha = 100.
-
-    call generaMatriz2(mat, term_ind, x0, x1, y0, y1, n, m, superior, inferior, izquierda, derecha)
+    call generarSistema(mat, term_ind, x0, x1, y0, y1, n, m, superior, inferior, izquierda, derecha)
     ! call mostrarMatriz(mat)
     ! write (*, *)
     ! call mostrarMatriz(term_ind)
@@ -39,50 +39,11 @@ program principal
     call mostrarMatriz(res)
     write (*, *)
 
-    ! Esquinas
-    distribucion(1, 1) = 0.
-    distribucion(1, n+1) = 100.
-    distribucion(m+1, 1) = 0.
-    distribucion(m+1, n+1) = 100.
-
-    ! Bordes
-    distribucion(1, 2:n) = superior
-    distribucion(m+1, 2:n) = inferior
-    distribucion(2:m, 1) = izquierda
-    distribucion(2:m, n+1) = derecha
-    offset = 0
-    do i = 2, m
-        distribucion(i, 2:n) = res(1+offset:n-1, 1)
-        offset = offset + n - 1
-    end do
-
+    distribucion = generarDistribucion(n, m, res(:, 1), 0._8, 100._8, 0._8, 100._8, superior, inferior, izquierda, derecha)
     ! call mostrarMatriz(distribucion)
-    call generarDatos(distribucion, 0._8, 20._8, 0._8, 10._8, n, m, 'valores.dat')
+    call grabarDatos(distribucion, x0, x1, y0, y1, n, m, 'valores.dat')
     call system('gnuplot -persist matriz.p')
 
     deallocate(mat, distribucion, superior, inferior, izquierda, derecha, term_ind, xini, res)
-contains
-
-    subroutine generarDatos(distribucion, x0, x1, y0, y1, n, m, archivo)
-        intent(in) :: distribucion, x0, x1, y0, y1, n, m, archivo
-        integer(4) n, m, i, j
-        real(8) distribucion(:, :), x0, x1, y0, y1, h, k, x, y
-        character(len=*) archivo
-
-        open(unit=2, file=archivo, access='SEQUENTIAL', status='REPLACE')
-        h = (x1 - x0) / n
-        k = (y1 - y0) / m
-        y = y0
-        do i = 1, m + 1
-            x = x0
-            do j = 1, n + 1
-                write(2, *) x, y, distribucion(i, j)
-                x = x + h
-            end do
-            write(2, *)
-            y = y + k
-        end do
-        close(2, status='KEEP')
-    end subroutine
 
 end program principal
