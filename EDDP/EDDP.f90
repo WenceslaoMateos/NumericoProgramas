8bd313b3818d02c0c220695c91af2e6bb6cb9f03
Bravlin <bravlinleonel@gmail.com>
2019-10-12 13:02:38 -0300

    Neumann

--- EDDP/EDDP.f90
+++ EDDP/EDDP.f90
@@ -1,6 +1,13 @@
 module EDDP
     implicit none
 
+    integer(4), parameter :: DIRICHLET = 0, NEUMANN = 1
+
+    type condicion
+        integer(4) tipo
+        real(8) valor
+    end type condicion
+
     abstract interface
         function poisson(x, y)
             real(8), intent(in) :: x, y
@@ -100,6 +107,120 @@ contains
         end do
     end subroutine generarSistema
 
+    subroutine generarSistema2(mat, term_ind, x0, x1, y0, y1, nx, my, superior, inferior, izquierda, derecha, f)
+        integer(4), intent(in) :: nx, my
+        real(8), intent(in) :: x0, x1, y0, y1
+        real(8), dimension(:, :), intent(out) :: mat, term_ind
+        type(condicion), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
+        procedure(poisson) :: f
+        integer(4) desde, hasta, i, n, m, offset
+        real(8) h, k, x, y
+
+        h = (x1 - x0) / nx
+        k = (y1 - y0) / my
+        n = nx - 1
+        m = my - 1
+        mat = 0.
+
+        ! Iniciacion de terminos independientes
+        x = x0
+        y = y1
+        do i = 1, size(term_ind, dim=1)
+            if (mod(i, n) == 1) then
+                x = x0 + h
+                y = y - k
+            else
+                x = x + h
+            end if
+            term_ind(i, :) = f(x, y) * h**2. * k**2
+        end do
+
+        ! ---------------GENERACION DE TERMINOS INDEPENDIENTES--------------- !
+        ! Condiciones superiores
+        desde = 1
+        hasta = n
+        do i = desde, hasta
+            if (superior(i)%tipo == DIRICHLET) then
+                term_ind(i, :) = term_ind(i, :) - superior(i)%valor * h**2.
+            elseif (superior(i)%tipo == NEUMANN) then
+                term_ind(i, :) = term_ind(i, :) - 2. * k * superior(i)%valor
+                mat(i, i + n) = h**2.
+            end if
+        end do
+
+        ! Condiciones inferiores
+        desde = n * (m - 1) + 1
+        hasta = n * m
+        offset = 0
+        do i = desde, hasta
+            offset = offset + 1
+            if (inferior(offset)%tipo == DIRICHLET) then
+                term_ind(i, :) = term_ind(i, :) - inferior(offset)%valor * h**2.
+            elseif (inferior(offset)%tipo == NEUMANN) then
+                term_ind(i, :) = term_ind(i, :) + 2. * k * inferior(offset)%valor
+                mat(i, i - n) = h**2.
+            end if
+        end do
+
+        ! Condiciones izquierda
+        desde = 1
+        hasta = n * (m - 1) + 1
+        offset = 0
+        do i = desde, hasta, n
+            offset = offset + 1
+            if (izquierda(offset)%tipo == DIRICHLET) then
+                term_ind(i, :) = term_ind(i, :) - izquierda(offset)%valor * k**2.
+            elseif (izquierda(offset)%tipo == NEUMANN) then
+                term_ind(i, :) = term_ind(i, :) + 2. * h * izquierda(offset)%valor
+                mat(i, i + 1) = k**2.
+            end if
+        end do
+        
+        ! Condiciones derecha
+        desde = n
+        hasta = n * m
+        offset = 0
+        do i = desde, hasta, n
+            offset = offset + 1
+            if (derecha(offset)%tipo == DIRICHLET) then
+                term_ind(i, :) = term_ind(i, :) - derecha(offset)%valor * k**2.
+            elseif (derecha(offset)%tipo == NEUMANN) then
+                term_ind(i, :) = term_ind(i, :) - 2. * h * derecha(offset)%valor
+                mat(i, i - 1) = k**2.
+            end if
+        end do
+
+        ! ----------------------GENERACION DE DIAGONAL---------------------- !
+        do i = 1, n * m
+            mat(i, i) = -2. * (h**2. + k**2.)
+        end do
+
+        ! ------------------------GENERACION DE UNOS------------------------ !
+        ! Banda superiores
+        do i = n + 1, n * m
+            mat(i, i - n) = mat(i, i - n) + h**2.
+        end do
+
+        ! Banda inferiores
+        do i = 1, n * (m - 1)
+            mat(i, i + n) = mat(i, i + n) + h**2.
+        end do
+
+        ! Banda izquierda
+        do i = 2, n * m
+            if (mod(i, n) /= 1) then
+                mat(i, i - 1) = mat(i, i - 1) + k**2.
+            end if
+        end do
+
+        ! Banda derecha
+        do i = 1, n * m - 1
+            if (mod(i, n) /= 0) then
+                mat(i, i + 1) = mat(i, i + 1) + k**2.
+            end if
+        end do
+    end subroutine generarSistema2
+
     function generarDistribucion(nx, my, resul, si, sd, ii, id, superior, inferior, izquierda, derecha)
         integer(4), intent(in) :: nx, my
         real(8), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
@@ -127,6 +248,56 @@ contains
         end do
     end function generarDistribucion
 
+    function generarDistribucion2(nx, my, resul, si, sd, ii, id, superior, inferior, izquierda, derecha)
+        integer(4), intent(in) :: nx, my
+        type(condicion), dimension(:), intent(in) :: superior, inferior, izquierda, derecha
+        real(8), intent(in) :: resul(1:(nx - 1) * (my - 1)), si, sd, ii, id
+        real(8) generarDistribucion2(1:my+1, 1:nx+1)
+        integer(4) i, offset
+
+        ! Interno
+        offset = 0
+        do i = 2, my
+            generarDistribucion2(i, 2:nx) = resul(1+offset:nx-1)
+            offset = offset + nx - 1
+        end do
+
+        ! Esquinas
+        generarDistribucion2(1, 1) = si
+        generarDistribucion2(1, nx+1) = sd
+        generarDistribucion2(my+1, 1) = ii
+        generarDistribucion2(my+1, nx+1) = id
+
+        ! Bordes
+        do i = 2, nx
+            if (superior(i - 1)%tipo == DIRICHLET) then
+                generarDistribucion2(1, i) = superior(i - 1)%valor
+            elseif (superior(i - 1)%tipo == NEUMANN) then
+                generarDistribucion2(1, i) = generarDistribucion2(3, i)
+            end if
+
+            if (inferior(i - 1)%tipo == DIRICHLET) then
+                generarDistribucion2(my + 1, i) = inferior(i - 1)%valor
+            elseif (inferior(i - 1)%tipo == NEUMANN) then
+                generarDistribucion2(my + 1, i) = generarDistribucion2(my - 1, i)
+            end if
+        end do
+
+        do i = 2, my
+            if (izquierda(i - 1)%tipo == DIRICHLET) then
+                generarDistribucion2(i, 1) = izquierda(i - 1)%valor
+            elseif (izquierda(i - 1)%tipo == NEUMANN) then
+                generarDistribucion2(i, 1) = generarDistribucion2(i, 3)
+            end if
+
+            if (derecha(i - 1)%tipo == DIRICHLET) then
+                generarDistribucion2(i, nx + 1) = derecha(i - 1)%valor
+            else if (derecha(i - 1)%tipo == NEUMANN) then
+                generarDistribucion2(i, nx + 1) = generarDistribucion2(i, nx - 1)
+            end if
+        end do
+    end function generarDistribucion2
+
     subroutine grabarDatos(distribucion, x0, x1, y0, y1, nx, my, archivo)
         intent(in) :: distribucion, x0, x1, y0, y1, nx, my, archivo
         integer(4) nx, my, i, j
@@ -136,7 +307,7 @@ contains
         open(unit=2, file=archivo, access='SEQUENTIAL', status='REPLACE')
         h = (x1 - x0) / nx
         k = (y1 - y0) / my
-        y = y0
+        y = y1
         do i = 1, my + 1
             x = x0
             do j = 1, nx + 1
@@ -144,7 +315,7 @@ contains
                 x = x + h
             end do
             write(2, *)
-            y = y + k
+            y = y - k
         end do
         close(2, status='KEEP')
     end subroutine
--- EDDP/main.f90
+++ EDDP/main.f90
@@ -9,42 +9,64 @@ program principal
     integer(4) n, m, orden
     real(8), dimension(:, :), allocatable :: mat, distribucion
     real(8), dimension(:), allocatable :: superior, inferior, izquierda, derecha
+    type(condicion), dimension(:), allocatable :: superior2, inferior2, izquierda2, derecha2
     real(8), dimension(:, :), allocatable :: term_ind, xini, res
 
     x0 = 0.
     x1 = 20.
     y0 = 0.
     y1 = 10.
-    n = 32
-    m = 16
+    n = 8
+    m = 4
     orden = (n - 1) * (m - 1)
 
     allocate(mat(1:orden, 1:orden), distribucion(1:m+1, 1:n+1))
     allocate(term_ind(1:orden, 1), xini(1:orden, 1), res(1:orden, 1))
     allocate(superior(1:n-1), inferior(1:n-1))
     allocate(izquierda(1:m-1), derecha(1:m-1))
+    allocate(superior2(1:n-1), inferior2(1:n-1))
+    allocate(izquierda2(1:m-1), derecha2(1:m-1))
 
     superior = 0.
     inferior = 0.
     izquierda = 0.
     derecha = 100.
-    call generarSistema(mat, term_ind, x0, x1, y0, y1, n, m, superior, inferior, izquierda, derecha, laplace)
+    !call generarSistema(mat, term_ind, x0, x1, y0, y1, n, m, superior, inferior, izquierda, derecha, laplace)
     ! call mostrarMatriz(mat)
     ! write (*, *)
     ! call mostrarMatriz(term_ind)
     ! write (*, *)
     
+    superior2%valor = 0.
+    inferior2%valor = 0.
+    izquierda2%valor = 0.
+    derecha2%valor = 100.
+    superior2%tipo = NEUMANN
+    inferior2%tipo = DIRICHLET
+    izquierda2%tipo = NEUMANN
+    derecha2%tipo = DIRICHLET
+    call generarSistema2(mat, term_ind, x0, x1, y0, y1, n, m, superior2, inferior2, izquierda2, derecha2, laplace)
+
+    call mostrarMatriz(mat, '(21F7.2)')
+    write (*, *)
+    call mostrarMatriz(term_ind, '(F7.2)')
+    write (*, *)
+
     xini = 0.
     res = gaussSeidel(mat, term_ind, xini, 0.000001_8)
     call mostrarMatriz(res)
     write (*, *)
 
-    distribucion = generarDistribucion(n, m, res(:, 1), 0._8, 100._8, 0._8, 100._8, superior, inferior, izquierda, derecha)
+    ! distribucion = generarDistribucion(n, m, res(:, 1), 0._8, 100._8, 0._8, 100._8, superior, inferior, izquierda, derecha)
     ! call mostrarMatriz(distribucion)
+
+    distribucion = generarDistribucion2(n, m, res(:, 1), 0._8, 100._8, 0._8, 100._8, superior2, inferior2, izquierda2, derecha2)
+    call mostrarMatriz(distribucion, '(9F7.2)')
     call grabarDatos(distribucion, x0, x1, y0, y1, n, m, 'valores.dat')
     call system('gnuplot -persist matriz.p')
 
     deallocate(mat, distribucion, superior, inferior, izquierda, derecha, term_ind, xini, res)
+    deallocate(superior2, inferior2, derecha2, izquierda2)
 
 contains
 
@@ -54,5 +76,19 @@ contains
 
         f = x / y
     end function
+
+    subroutine plot(archivo)
+        character(len=*), intent(in) :: archivo
+
+        open(unit=2, file="temporal.p", access='SEQUENTIAL', status='REPLACE')
+        write(2, *) 'set title "Distribución de Temperaturas en una placa rectangular"'
+        write(2, *) 'set xlabel "x"'
+        write(2, *) 'set ylabel "y"'
+        write(2, *) 'set pm3d map'
+        write(2, *) 'set nokey'
+        write(2, *) 'splot "', archivo, '"'
+        call system('gnuplot -persist temporal.p')
+        close(2, STATUS='DELETE')
+    end subroutine plot
 
 end program principal
