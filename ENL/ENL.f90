dad135a3299452233194cf1dc08784bbc24ff0e9
Wenceslao <wenceslaomateos@gmail.com>
2019-11-05 16:56:53 -0300

    ENL completos by OTMFW
    Co-authored-by: Bravlin <bravlinleonel@gmail.com>

--- ENL/ENL.f90
+++ ENL/ENL.f90
@@ -1,6 +1,104 @@
 module ENL
     implicit none
     
+    abstract interface
+        function funcion(x)
+            real(8), intent(in) :: x
+            real(8) funcion
+        end function
+    end interface
+
 contains
     
+    function biseccion(a, b, f, tol)
+        real(8), intent(in) :: a, b, tol
+        procedure(funcion) :: f
+        real(8) biseccion, izq, der
+        integer(4) n, i
+
+        izq = a
+        der = b
+        n = ceiling((log(abs(der - izq)/ tol))/log(2.))
+        do i = 1, n
+            biseccion = (izq + der)/2.
+            if (f(izq) * f(biseccion) < 0.) then
+                der = biseccion
+            else
+                izq = biseccion
+            end if
+        end do
+    end function biseccion
+
+    function puntoFijo(x0, f, g, tol, max_iter)
+        real(8), intent(in) :: x0, tol
+        integer(4), intent(in) :: max_iter
+        procedure(funcion) :: f, g
+        integer(4) i
+        real(8) error, x, puntoFijo
+        
+        x = x0
+        error = tol + 1
+        i = 0
+        do while ((error >= tol) .AND. (i <= max_iter))
+            x = g(x)
+            error = abs(f(x))
+            i = i + 1
+        end do
+        puntoFijo = x
+    end function puntoFijo
+
+    function puntoFijoSistematico(x0, f, lambda, tol, max_iter)
+        real(8), intent(in) :: x0, tol
+        integer(4), intent(in) :: max_iter
+        procedure(funcion) :: f
+        integer(4) i
+        real(8) error, x, puntoFijoSistematico, lambda
+        
+        x = x0
+        error = tol + 1
+        i = 0
+        do while ((error >= tol) .AND. (i <= max_iter))
+            x = x - lambda * f(x)
+            error = abs(f(x))
+            i = i + 1
+        end do
+        puntoFijoSistematico = x
+    end function puntoFijoSistematico
+
+    function newton(x0, f, df, tol, max_iter)
+        real(8), intent(in) :: x0, tol
+        integer(4), intent(in) :: max_iter
+        procedure(funcion) :: f, df
+        integer(4) i
+        real(8) error, x, newton
+        
+        x = x0
+        error = tol + 1
+        i = 0
+        do while ((error >= tol) .AND. (i <= max_iter))
+            x = x - f(x)/df(x)
+            error = abs(f(x))
+            i = i + 1
+        end do
+        newton = x
+    end function newton
+
+    subroutine plot(f, nombre, rango)
+        character (LEN=*), intent(in) :: f, rango, nombre
+        optional rango
+
+        open(unit=2, file="temporal.p", access='SEQUENTIAL', status='REPLACE')
+        write(2, *) "set autoscale"
+        write(2, *) "unset log"
+        write(2, *) "unset label"
+        write(2, *) "set grid"
+        write(2, *) "set xtic auto"
+        write(2, *) "set ytic auto"
+        write(2, *) "set title 'Spline'"
+        write(2, *) "set xlabel 'x'"
+        write(2, *) "set ylabel 'f(x)'"
+        write(2, *) "plot ", rango ," ", f ," title '", nombre ,"' with lines"
+        call system('gnuplot -persist temporal.p')
+        close(2, STATUS='DELETE')
+    end subroutine plot
 end module ENL
--- ENL/main.f90
+++ ENL/main.f90
@@ -1,5 +1,76 @@
 program main
-    implicit none
     use ENL
+    implicit none
+    real(8) a, b, tol, res, x0, lambda
     
+    !call plot("cos(x)*cosh(x)+1", "[4:5]")
+    a = -0.3
+    b = -0.1
+    tol = 1e-5
+    res = biseccion(a, b, func, tol)
+    write(*, *) "El resultado de biseccion es: ", res
+    
+    x0 = -0.99
+    tol = 1e-5
+    res = puntoFijo(x0, func, g, tol, 200)
+    write(*, *) "El resultado de punto fijo es: ", res
+
+    a = -0.3
+    b = -0.1
+    x0 = -0.99
+    tol = 1e-5
+    !El lambda a elegir depende del intervalo a buscar raiz.
+    lambda = 1./maxDeriv(a, b, dfunc, tol)
+    res = puntoFijoSistematico(x0, func, lambda, tol, 200)
+    write(*, *) "El resultado de punto fijo sistematico es: ", res
+
+    x0 = -0.3
+    tol = 1e-5
+    res = newton(x0, func, dfunc, tol, 200)
+    write(*, *) "El resultado de newton es: ", res
+
+    !call plot("((x+1)/(x-1))-sin(3*x)", "f(x)", "[-5:5]")
+    !call plot("asin((x+1.)/(x-1.))/3.", "g(x)", "[-5:5]")
+    call plot("-2./(x-1)**2-3.*cos(3*x)", "Df(x)", "[-0.3:-0.1]")
+
+
+    contains
+
+    function func(x)
+        real(8), intent(in) :: x
+        real(8) func
+
+        func = ((x+1)/(x-1)) - sin(3*x)
+    end function func
+
+    function dfunc(x)
+        real(8), intent(in) :: x
+        real(8) dfunc
+
+        dfunc = -2./(x-1)**2 - 3. *cos(3*x)
+    end function dfunc
+
+    function g(x)
+        real(8), intent(in) :: x
+        real(8) g
+
+        g = asin((x+1.)/(x-1.))/3.
+    end function g
+
+    function maxDeriv(a, b, deriv, h)
+        real(8), intent(in) :: a, b, h
+        real(8) maxDeriv, x, act
+        procedure(funcion) :: deriv
+
+        maxDeriv = deriv(a)
+        x = a + h
+        do while (x <= b)
+            act = deriv(x)
+            if (act > maxDeriv) then
+                maxDeriv = act
+            end if
+            x = x + h
+        end do
+    end function maxDeriv
+
 end program main
