3f23ab68955bc0fbb7dc515535d87b1123e321f4
Wenceslao <wenceslaomateos@gmail.com>
2019-11-10 15:13:14 -0300

    otro ejemplo mas e implementacion de la derivada

--- Interpolacion/main.f90
+++ Interpolacion/main.f90
@@ -3,12 +3,16 @@ program main
 
     implicit none
     
-    real(8), dimension(0:6) :: a, b, c, d, x, y, lagrange, polinomioDescendente, polinomioAscendente, directo
-    real(8), dimension(0:5) :: h
+    real(8), dimension(0:5) :: a, b, c, d, x, y, lagrange, polinomioDescendente, polinomioAscendente, directo
+    real(8), dimension(0:4) :: h
     real(8), dimension(:), allocatable :: minCuad
-    real(8) xdesco
+    real(8) xdesco, m
     
-    xdesco = 5.35
+    xdesco = 0.25
+    x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    y = [1., 0.99500, 0.98006, 0.95533, 0.92106, 0.87758]
+    !x = [0.1, 0.2, 0.3, 0.4, 0.5]
+    !y = [1.302, 1.616, 1.954, 2.328, 2.750]
     !x = [-4., -2., 0., 2.]
     !y = [-7.38, 0.52, 2., 14.52]
     !x = [0., 2., 4., 6.]
@@ -19,21 +23,29 @@ program main
     !para splines
     !x = [27.70, 28., 29., 30.]
     !y = [4.10, 4.30, 4.10, 3.00]
-    x = [17.00, 20.00, 23.00, 24.00, 25.00, 27.00, 27.70]
-    y = [4.50, 7., 6.10, 5.60, 5.80, 5.20, 4.10]
+    !x = [17.00, 20.00, 23.00, 24.00, 25.00, 27.00, 27.70]
+    !y = [4.50, 7., 6.10, 5.60, 5.80, 5.20, 4.10]
     !x = [1.00, 2.00, 5.00, 6.00, 7.00, 8.00, 10.00, 13.00, 17.00]
     !y = [3.00, 3.70, 3.90, 4.20, 5.70, 6.60, 7.10, 6.70, 4.50]
-    a = y
+    !a = y
     
     !directo = polinomioAproximante(x, y)
     !lagrange = polinomioLagrange(x, y)
     !call mejorMinimosCuadrados(x, y, minCuad, varianza)
-    !call diferenciasDivididas(x, y, polinomioDescendente, polinomioAscendente)
-    h = calculaH(x)
-    call splinesCubicos(a, b, c, d, h)
-    !write(*, *) 'Divididas Ascendente: ',evaluarPolinomio(polinomioAscendente, xdesco)
-    !write(*, *) 'Divididas Descendente: ',evaluarPolinomio(polinomioDescendente, xdesco)
-    !write(*, *) 'Equiespaciado: ',diferenciasEquiespaciado(x, y, xdesco)
+    call diferenciasDivididas(x, y, polinomioDescendente, polinomioAscendente)
+    !h = calculaH(x)
+    !call splinesCubicos(a, b, c, d, h)
+    write(*, *) 'Divididas Ascendente: ', evaluarPolinomio(polinomioAscendente, xdesco)
+    write(*, *) 'Error: ', calculaError(xdesco, x, derivadaNmasuno)
+    write(*, *)
+
+    write(*, *) 'Divididas Descendente: ',evaluarPolinomio(polinomioDescendente, xdesco)
+    write(*, *) 'Error: ', calculaError(xdesco, x, derivadaNmasuno)
+    write(*, *)
+    
+    write(*, *) 'Equiespaciado: ',diferenciasEquiespaciado(x, y, xdesco)
+    write(*, *) 'Error: ', calculaError(xdesco, x, derivadaNmasuno)
+    write(*, *)
     !write(*, *) 'Lagrange: ',evaluarPolinomio(lagrange, xdesco)
     !write(*, *) 'Directo: ',evaluarPolinomio(directo, xdesco)
     !write(*, *) 'Splines: ',evaluarSplines(y, b, c, d, x, xdesco)
@@ -44,9 +56,16 @@ program main
     !call graficarPolinomio(minCuad, x, 0.01_8, 'archivo.dat', 'Minimos Cuadrados')
     !call graficarPolinomio(lagrange, x, 0.01_8, 'archivo.dat', 'Lagrange')
 
-    call graficarSplines(a, b, c, d, x, 0.01_8, 'archivo.dat', 'Splines')
+    !call graficarSplines(a, b, c, d, x, 0.01_8, 'archivo.dat', 'Splines')
     
 contains
+
+    function derivadaNmasuno(x)
+        real(8), intent(in) :: x
+        real(8) derivadaNmasuno
+
+        derivadaNmasuno = sin(x)
+    end function derivadaNmasuno
 
     function calculaH(x)
         real(8), dimension(0:) :: x
